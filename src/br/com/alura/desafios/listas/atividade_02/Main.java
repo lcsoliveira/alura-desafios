16d4428f63cffc218fed1f0fcd591d7658cb177d
Lucas Oliveira <lucas_prog@hotmail.com>
2025-02-10 20:58:37 -0300

    add exercicio 03
    Modifique o Exercício 2 para incluir uma verificação usando instanceof para garantir que o objeto seja do tipo correto antes de fazer o casting.

--- src/br/com/alura/desafios/listas/atividade_02/Main.java
+++ src/br/com/alura/desafios/listas/atividade_02/Main.java
@@ -2,9 +2,12 @@ package br.com.alura.desafios.listas.atividade_02;
 
 public class Main {
     public static void main(String[] args) {
-        Cachorro cachorro = new Cachorro();
-        Animal animal = (Animal) cachorro;
+        Animal animal = new Cachorro();
 
+        if (animal instanceof Cachorro) {
+            Cachorro cachorro = (Cachorro) animal;
+            System.out.println("É um cachorro");
+        }
 
 
     }
